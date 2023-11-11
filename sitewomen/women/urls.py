cfea99ed59f0e0b15345e16b02cd8b612b52261a
Nudlik <gegorp@mail.com>
2023-11-11 12:20:08 +0700

    1.6 динамические урлы

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, register_converter
+
+from . import converter
 from . import views
 
+
+register_converter(converter.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('about/', views.about),
     path('women/cat/', views.catalog),
     path('', views.index),
-    path('cats/', views.categories),
+    path('cats/<int:cat_id>', views.categories),
+    path('cats/<slug:cat_slug>', views.categories_by_slug),
+    path('archive/<year4:year>', views.archive),
 ]
