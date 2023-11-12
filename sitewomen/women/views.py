e80f87a512f330da65ef41541837a12f5c83c243
Nudlik <gegorp@mail.com>
2023-11-12 11:56:07 +0700

    2.4 тэги шаблонов

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,11 +7,18 @@ from django.urls import reverse
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': True},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
 
 def index(request: HttpRequest) -> HttpResponse:
     data = {
         'title': 'Главная страница',
         'menu': menu,
+        'posts': [i for i in data_db if i['is_published']],
     }
     return render(request, 'women/index.html', context=data)
 
