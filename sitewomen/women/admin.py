669612c1a184d831f346d56a621b4c8524822c2f
Nudlik <gegorp@mail.com>
2023-11-23 19:51:12 +0700

    6.4 Панель поиска и панель фильтрации

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -3,15 +3,33 @@ from django.contrib import admin, messages
 from women.models import Women, Category, TagPost, Husband
 
 
+class MariedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        if self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ['title', 'time_create', 'is_published', 'brief_info', 'count_tags']
+    list_display = ['title', 'cat', 'time_create', 'is_published', 'brief_info', 'count_tags']
     list_display_links = ['title']
     ordering = ['-time_create', 'title']
-    search_fields = ['title', 'content']
+    search_fields = ['title', 'cat__name']
     list_editable = ['is_published']
     list_per_page = 10
     actions = ['set_published', 'set_draft']
+    list_filter = ['is_published', MariedFilter, 'cat']
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women):
