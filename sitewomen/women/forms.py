3a0b461d956d8b2c600fd2ee7f02d471b45177d5
Nudlik <gegorp@mail.com>
2023-11-25 22:20:42 +0700

    7.3 Отображение полей формы. Сохранение переданных данных в БД

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -5,9 +5,11 @@ from .models import Category, Husband, TagPost
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255, label='Заголовок')
     slug = forms.SlugField(max_length=255, label='Слаг')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Содержимое')
-    is_published = forms.BooleanField(label='Опубликовано', required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория')
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Супруги')
-    tags = forms.ModelMultipleChoiceField(queryset=TagPost.objects.all(), required=False, label='Теги')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5, 'placeholder': 'Содержимое статьи'}),
+                              required=False, label='Содержимое')
+    is_published = forms.BooleanField(label='Опубликовано', initial=True, required=False)
 
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категория')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), empty_label='Не замужем',
+                                     required=False, label='Супруги')
+    tags = forms.ModelMultipleChoiceField(queryset=TagPost.objects.all(), required=False, label='Теги')
