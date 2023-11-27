e7c4c331363d777cf8c181b43b85ceb35e6896f9
Nudlik <gegorp@mail.com>
2023-11-27 16:31:58 +0700

    7.8 Отображение изображений в HTML-документе и админ-панели

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from sitewomen import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Панель администрирования'
 admin.site.index_title = 'Известные женщины мира'
