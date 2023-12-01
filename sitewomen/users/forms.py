72c72bc1377c9c9466142bd398e15506affc0bcc
Nudlik <gegorp@mail.com>
2023-12-01 17:04:38 +0700

    9.6 Регистрация пользователей через функции представления

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -4,12 +4,15 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class MixinWidgets:
-    widgets = None
+
+    class Meta:
+        widgets = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.widgets:
-            for field_name, widget in self.widgets.items():
+        widgets = self.Meta.widgets
+        if widgets:
+            for field_name, widget in widgets.items():
                 self.fields[field_name].widget = widget
 
 
@@ -17,10 +20,41 @@ class UserLoginForm(MixinWidgets, AuthenticationForm):
     username = forms.CharField(label='Логин')
     password = forms.CharField(label='Пароль')
 
-    widgets = {
-        'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваш логин'}),
-        'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Ваш пароль'}),
-    }
+    class Meta:
+        model = get_user_model()
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваш логин'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Ваш пароль'}),
+        }
+
+
+class RegisterUserForm(MixinWidgets, forms.ModelForm):
+    username = forms.CharField(label='Логин')
+    password = forms.CharField(label='Пароль')
+    password2 = forms.CharField(label='Повтор пароля')
 
     class Meta:
         model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+        }
+        widgets = {
+            'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Придумайте пароль'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повторите пароль'}),
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        password, password2 = cd['password'], cd['password2']
+        if password != password2:
+            raise forms.ValidationError('Пароли не совпадают')
+        return password2
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с таким E-mail уже существует')
+        return email
--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, RegisterUserForm
 
 
 class LoginUserView(LoginView):
@@ -9,3 +10,17 @@ class LoginUserView(LoginView):
     extra_context = {
         'title': 'Авторизация',
     }
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        form = RegisterUserForm()
+    return render(request, 'users/register.html', {'form': form})
