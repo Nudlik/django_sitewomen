60e39ffb7270ad106fd3ec9d67a0771a071ca3e1
Nudlik <gegorp@mail.com>
2023-12-04 08:18:10 +0700

    9.11 Реализация алгоритма восстановления пароля

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -11,6 +11,13 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUserView.as_view(), name='profile'),
+
     path('passwoed-change/', views.UserPasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', views.UserPasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password-reset/', views.UserPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, \
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
@@ -42,3 +43,40 @@ class UserPasswordChangeView(PasswordChangeView):
 
 class UserPasswordChangeDoneView(PasswordChangeDoneView):
     template_name = 'users/password_change_done.html'
+    extra_context = {
+        'title': 'Ваш пароль был успешно изменен!',
+        'button': 'Вернуться в профиль',
+    }
+
+
+class UserPasswordResetView(PasswordResetView):
+    template_name = 'users/password_reset_form.html'
+    email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    extra_context = {
+        'title': 'Восстановление пароля',
+        'button': 'Сбросить по E-mail',
+    }
+
+
+class UserPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'users/password_reset_done.html'
+    extra_context = {
+        'title': 'Сброс пароля',
+    }
+
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy('users:password_reset_complete')
+    extra_context = {
+        'title': 'Новый пароль',
+        'button': 'Сохранить',
+    }
+
+
+class UserPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'users/password_reset_complete.html'
+    extra_context = {
+        'title': 'Восстановление пароля завершено',
+    }
